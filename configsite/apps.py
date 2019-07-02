1aeebf969f40d304f2b4deab6beaa803d621e6b3
bennyman123abc <bennyman123abc@hotmail.com>
2019-07-02 13:17:59 -0500

    Create Apps and Setup URLs and Basic Views

---
+++ configsite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConfigsiteConfig(AppConfig):
+    name = 'configsite'
---
+++ wiistorage/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    print("Not Implemented LOOOOOOL")
+    # write the code to handle wii storage management here
+    # I'll do this if you want me to, Larsen
